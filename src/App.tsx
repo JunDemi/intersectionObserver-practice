7ff696f5db986c7da5a34094c2cea2a6a2a6a463
JunDemi <jungwook3176@gmail.com>
2024-08-07 13:34:59 +0900

    chore: 주석 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 
 function App() {
-  const {ref, inView} = useInView();
+  const { ref, inView } = useInView();
   //리액트 쿼리 훅
-  const {status, data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery({
+  const { status, data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery({
     queryKey: ["worldcup"],
     queryFn: fetchItems,
     initialPageParam: 0,
@@ -14,10 +14,10 @@ function App() {
   });
 
   useEffect(() => {
-    if(inView) {
+    if (inView) {
       fetchNextPage();
     }
-  },[inView, fetchNextPage]);
+  }, [inView, fetchNextPage]);
 
   return status === "pending" ? (
     <div>로딩 중...</div>
@@ -25,41 +25,24 @@ function App() {
     <div>데이터를 불러오지 못했습니다.</div>
   ) : (
     <div className="container">
-      {data?.pages.map((page) => {
-        return (
-          <div key={page.currentPage}>
-            {page.data.map((items) => (
-              <div key={items.id}>
-                <p>{items.data.name}</p>
-                <img
-                  src={items.data.worldcupImages[0].filePath}
-                  alt=""
-                  width="500px"
-                />
-              </div>
-            ))}
-          </div>
-        );
-        
-      })}
+      {data?.pages.map((page) => (
+        <div key={page.currentPage}>
+          {page.data.map((items) => (
+            <div key={items.id}>
+              <p>{items.data.name}</p>
+              <img
+                src={items.data.worldcupImages[0].filePath}
+                alt=""
+                width="500px"
+              />
+            </div>
+          ))}
+        </div>
+      ))}
 
-<div ref={ref}>{isFetchingNextPage && "불러오는중..."}</div>
+      <div ref={ref}>{isFetchingNextPage && "불러오는중..."}</div>
     </div>
   );
-
-  // <section className="container">
-  //       {data.pages[data?.pageParams.length - 1]?.map((data, number) => (
-  //         <div key={number}>
-  //           <p>{data.data.worldcupTitle}</p>
-  //           <img
-  //             src={data.data.worldcupImages[0].filePath}
-  //             alt=""
-  //             width="500px"
-  //           />
-  //         </div>
-  //       ))}
-  //       <div ref={trigger} >hi</div>
-  //     </section>
 }
 
 export default App;
